57a7a1a4b83a3d70b238a6d2c90e4f2b83330256
gowrish005 <gowrishhb5@gmail.com>
2025-01-20 02:37:01 +0530

    added RM IPR + multiple Admin Pages like Control Panel

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -5,17 +5,29 @@ from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
+    # Main page
     path('', views.main_page, name='main'),
-    path('branches/<int:semester_id>/', views.branch_page, name='branches'),  # updated 'semester_id' to 'semester'
+
+    # Branches and subjects
+    path('branches/<int:semester_id>/', views.branch_page, name='branches'),
     path('subjects/<int:branch_id>/', views.subject_page, name='subjects'),
     path('modules/<int:subject_id>/', views.modules_page, name='modules'),
-    path('defualt', views.defualt, name='defualt'),  # fix typo from 'defualt' to 'default'
+
+    # Admin functionalities
     path('admins/uploads/', views.upload_module, name='admin_uploads'),
     path('admins/create-branch/', views.create_branch, name='create_branch'),
+    path('admins/control-panel/', views.admin_control_panel, name='admin_control_panel'),
+
+    # Creation functionalities
     path('create-branch/', views.create_branch, name='create_branch'),
     path('create-subject/', views.create_subject, name='create_subject'),
+
+    # Get branches and subjects
     path('get-branches/<int:semester_id>/', views.get_branches, name='get_branches'),
     path('get-subjects/<int:branch_id>/', views.get_subjects, name='get_subjects'),
+
+    # Default page
+    path('default', views.defualt, name='default'),  # fixed typo from 'defualt' to 'default'
 ]
 if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,6 +4,12 @@ from requests import request
 from .models import Semester, Branch, Subject, Module, TempModule, TempBranch, TempSubject
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+from django.contrib.admin.views.decorators import staff_member_required
+from natsort import natsorted #to fix the issue where 10 comes before 1 in the modules page
+
+def defualt(request):
+    return render(request, 'index.html')
+    
 
 def main_page(request):
     semesters = Semester.objects.all()
@@ -15,49 +21,54 @@ def branch_page(request, semester_id):
     return render(request, 'branches.html', {'branches': branches, 'semester_name': semester.name})
 
 def subject_page(request, branch_id):
-    subjects = Subject.objects.filter(branch_id=branch_id)
+    subjects = Subject.objects.filter(branch_id=branch_id).order_by('subjectcode')
     branch = get_object_or_404(Branch, id=branch_id)
     return render(request, 'subjects.html', {'subjects': subjects, 'branch': branch})
 
 def modules_page(request, subject_id):
     modules = Module.objects.filter(subject_id=subject_id)
+    modules = natsorted(modules, key=lambda x: x.name) #to fix the issue where 10 comes before 1 in the modules page
     subject = get_object_or_404(Subject, id=subject_id)
     subject_code = subject.subjectcode
     return render(request, 'modules.html', {'modules': modules, 'subject_name': subject.name, 'subject_code': subject_code})
 
-def defualt(request):
-    return render(request, 'index.html')
-    
-def upload_module(request):
-       if request.method == 'POST':
-           name = request.POST.get('name')
-           description = request.POST.get('description')
-           image = request.FILES.get('image')
-           file = request.FILES.get('file')
-           subject_id = request.POST.get('subject')
-           semester_id = request.POST.get('semester')
-           branch_id = request.POST.get('branch')
-           subject = get_object_or_404(Subject, id=subject_id)
-           semester = get_object_or_404(Semester, id=semester_id)
-           branch = get_object_or_404(Branch, id=branch_id)
-           temp_module = TempModule(
-               name=name,
-               description=description,
-               image=image,
-               file=file,
-               subject=subject,
-               semester=semester,
-               branch=branch
-           )
-           temp_module.save()
-           messages.success(request, "Module uploaded successfully and is pending approval.")
-           return redirect('main')
-       
-       semesters = Semester.objects.all()
-       branches = Branch.objects.none()
-       subjects = Subject.objects.all()
 
-       return render(request, 'upload_module.html', {'semesters': semesters, 'branches': branches, 'subjects': subjects})
+def upload_module(request):
+    if request.method == 'POST':
+        semester_id = request.POST.get('semester')
+        branch_id = request.POST.get('branch')
+        subject_id = request.POST.get('subject')
+        semester = get_object_or_404(Semester, id=semester_id)
+        branch = get_object_or_404(Branch, id=branch_id)
+        subject = get_object_or_404(Subject, id=subject_id)
+        
+        module_count = len([key for key in request.POST.keys() if key.startswith('module_name_')])
+        
+        for i in range(1, module_count + 1):
+            name = request.POST.get(f'module_name_{i}')
+            description = request.POST.get(f'module_description_{i}')
+            image = request.FILES.get(f'module_image_{i}')
+            file = request.FILES.get(f'module_file_{i}')
+            
+            temp_module = TempModule(
+                name=name,
+                description=description,
+                image=image,
+                file=file,
+                subject=subject,
+                semester=semester,
+                branch=branch
+            )
+            temp_module.save()
+        
+        messages.success(request, "Modules uploaded successfully and are pending approval.")
+        return redirect('main')
+    
+    semesters = Semester.objects.all()
+    branches = Branch.objects.none()
+    subjects = Subject.objects.all()
+
+    return render(request, 'upload_module.html', {'semesters': semesters, 'branches': branches, 'subjects': subjects})
 
 
 def create_branch(request):
@@ -72,7 +83,9 @@ def create_branch(request):
         return redirect('main')
     
     semesters = Semester.objects.all()
-    return render(request, 'create_branch.html', {'semesters': semesters})
+    branches = Branch.objects.all()
+    subjects = Subject.objects.all()
+    return render(request, 'create_branch.html', {'semesters': semesters, 'branches': branches, 'subjects': subjects})
 
 def create_subject(request):
     if request.method == 'POST':
@@ -88,8 +101,9 @@ def create_subject(request):
         return redirect('main')
     
     semesters = Semester.objects.all()
-    branches = Branch.objects.none()
-    return render(request, 'create_subject.html', {'semesters': semesters, 'branches': branches})
+    branches = Branch.objects.all()
+    subjects = Subject.objects.all()
+    return render(request, 'create_subject.html', {'semesters': semesters, 'branches': branches, 'subjects': subjects})
 
 def get_branches(request, semester_id):
     branches = Branch.objects.filter(semester_id=semester_id)
@@ -100,3 +114,16 @@ def get_subjects(request, branch_id):
     subjects = Subject.objects.filter(branch_id=branch_id)
     subjects_data = [{'id': subject.id, 'name': subject.name} for subject in subjects]
     return JsonResponse({'subjects': subjects_data})
+
+@staff_member_required
+def admin_control_panel(request):
+    semesters = Semester.objects.all()
+    branches = Branch.objects.all().order_by('name', 'semester__name')
+    subjects = Subject.objects.all()
+    modules = Module.objects.all()
+    return render(request, 'admin_control_panel.html', {
+        'semesters': semesters,
+        'branches': branches,
+        'subjects': subjects,
+        'modules': modules
+    })
